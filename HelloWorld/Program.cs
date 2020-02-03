83ff7a73abd2dd03e5a4c5034051f2da737fc547
Barnaby Gage <btgage@socnet.soc.plymouth.ac.uk>
2020-02-03 17:51:30 +0000

    partway completed task1, polymorphism left to go

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DepartmentOfTransport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("code running: project version " + RoadVehicle.ProjectVersion);
+
+            RoadVehicle v1 = new RoadVehicle(EngineSerialNumber:123);
+            Console.WriteLine(v1.Description);
+
+
+            RoadVehicle v2 = new RoadVehicle(EngineSerialNumber:456);
+            Console.WriteLine(v2.Description);
+
+            if ((v1 == null) || (v2 == null))
+            {
+                Console.WriteLine("borked");
+                return;
+            }
+
+            Car PrimaryCar = new Car(EngineSerialNumber:999);
+            PrimaryCar.HasTowBar = true;
+            Console.WriteLine(PrimaryCar.Description);
+
+        }
+    }
+}
