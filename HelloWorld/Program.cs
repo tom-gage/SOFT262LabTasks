ee5848b03e5bf4007d4245586eb948e613f1675d
Barnaby Gage <btgage@socnet.soc.plymouth.ac.uk>
2020-02-04 19:03:54 +0000

    finished hello world task, ch1 intro complete

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,26 +6,13 @@ namespace DepartmentOfTransport
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("code running: project version " + RoadVehicle.ProjectVersion);
+            Driver commuter1 = new Driver(Name: "Regular Dave", new Car(EngineSerialNumber: 12345, HasTowBarFitted: true));
+            Driver commuter2 = new Driver(Name: "Cool Dave", new Motorbike(EngineSerialNumber: 333555));
+            Driver commuter3 = new Driver(Name: "Peasant Dave", null);
 
-            RoadVehicle v1 = new RoadVehicle(EngineSerialNumber:123);
-            Console.WriteLine(v1.Description);
-
-
-            RoadVehicle v2 = new RoadVehicle(EngineSerialNumber:456);
-            Console.WriteLine(v2.Description);
-
-            if ((v1 == null) || (v2 == null))
-            {
-                Console.WriteLine("borked");
-                return;
-            }
-
-            Car PrimaryCar = new Car(EngineSerialNumber:999);
-            PrimaryCar.HasTowBar = true;
-            Console.WriteLine(PrimaryCar.Description);
-
+            Console.WriteLine(commuter1.Description);
+            Console.WriteLine(commuter2.Description);
+            Console.WriteLine(commuter3.Description);
         }
     }
 }
